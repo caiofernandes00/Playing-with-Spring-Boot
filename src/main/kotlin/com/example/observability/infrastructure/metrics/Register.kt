b69ddf51a47a8ed45bce5b76e0bde92d1456599d
Caio Fernandes <caiow.wk@gmail.com>
2023-01-29 14:59:34 -0300

    feat: add missing @configuration bean

--- src/main/kotlin/com/example/observability/infrastructure/metrics/Register.kt
+++ src/main/kotlin/com/example/observability/infrastructure/metrics/Register.kt
@@ -4,7 +4,9 @@ import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
+@Configuration
 class Register {
 
     @Bean
